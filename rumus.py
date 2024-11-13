30dc8d73b0fa732ef98dad72b1dd1dcb86c0faa7
Esto Triramdani N <60980963+estotriramdani@users.noreply.github.com>
2024-11-13 14:06:20 +0700

    Add files via upload

---
+++ rumus.py
@@ -0,0 +1,9 @@
+import math
+
+# Fungsi untuk menghitung keliling segitiga sama sisi
+def keliling_segitiga(s):
+    return 3 * s
+
+# Fungsi untuk menghitung luas segitiga sama sisi
+def luas_segitiga(s):
+    return (s**2 * math.sqrt(3)) / 4
